d317b046067d09d9bf6f2b4ddb30ebdd0da9ad51
Frances Hughes <franceslhughes@hotmail.com>
2024-02-29 14:48:52 +1300

    made minor changes to VideoCard and Navbar
    made minor changes to VideoCard and Navbar. Now grabs videos from API

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Card, Typography, CardMedia, CardContent } from "@mui/material";
+import { Link } from "react-router-dom";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import {
+  demoVideoUrl,
+  demoThumbnailUrl,
+  demoChannelUrl,
+  demoVideoTitle,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
